8b031bf3f8133406c9b8545feee99d93544aaf4e
nicolassqueren <40861762+nicolassqueren@users.noreply.github.com>
2018-07-15 09:49:01 +0200

    writter

--- typeWritter.js
+++ typeWritter.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function (event) {
     // array with texts to type in typewriter 
-    var dataText = ["salut slaut"];
+    var dataText = ["Développeur WEB"];
     
     // type one text in the typwriter
     // keeps calling itself until the text is finished
